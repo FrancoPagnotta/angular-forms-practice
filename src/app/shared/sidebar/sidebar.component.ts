761c90fd674912ea8edc16981b920f05b0c114d8
Franco Pagnotta <francoapagnotta@gmail.com>
2022-05-21 19:17:33 -0300

    create side menulinks

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,11 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+interface MenuItem {
+  text: string;
+  route: string;
+}
+
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent implements OnInit {
+  menu: MenuItem[] = [
+    {
+      text: 'basics',
+      route: './template/basics'
+    },
+    {
+      text: 'dynamics',
+      route: './template/dynamics'
+    },
+    {
+      text: 'switches',
+      route: './template/switches'
+    },
+  ]
 
   constructor() { }
 
