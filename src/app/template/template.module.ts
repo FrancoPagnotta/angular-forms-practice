c60256914bec2f8e5f6fd3d9f3c18f1aec673227
Franco Pagnotta <francoapagnotta@gmail.com>
2022-05-21 01:33:59 -0300

    create basics component

--- src/app/template/template.module.ts
+++ src/app/template/template.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TemplateRoutingModule } from './template-routing.module';
+import { BasicsComponent } from './basics/basics.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BasicsComponent
+  ],
   imports: [
     CommonModule,
     TemplateRoutingModule
