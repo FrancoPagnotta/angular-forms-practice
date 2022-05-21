842b16bcfd286d6a3ca0a77a51bdcd626ee53939
Franco Pagnotta <francoapagnotta@gmail.com>
2022-05-21 01:20:01 -0300

    create dynamics component

--- src/app/reactive/reactive.module.ts
+++ src/app/reactive/reactive.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { ReactiveRoutingModule } from './reactive-routing.module';
 import { BasicsComponent } from './basics/basics.component';
+import { DynamicsComponent } from './dynamics/dynamics.component';
 
 
 @NgModule({
   declarations: [
-    BasicsComponent
+    BasicsComponent,
+    DynamicsComponent
   ],
   imports: [
     CommonModule,
