7679d4f0b34231f575d45ac92a407936ed708808
Franco Pagnotta <francoapagnotta@gmail.com>
2022-05-21 20:13:06 -0300

    create template form with local reference for the form and ngModel for inputs

--- src/app/template/template.module.ts
+++ src/app/template/template.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { FormsModule } from '@angular/forms';
 import { TemplateRoutingModule } from './template-routing.module';
+
 import { BasicsComponent } from './basics/basics.component';
 import { DynamicsComponent } from './dynamics/dynamics.component';
 import { SwitchesComponent } from './switches/switches.component';
-import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
