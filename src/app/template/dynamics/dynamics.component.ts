bc4e61200e7433e774b62e160cf5a4ac97e78cfc
Franco Pagnotta <francoapagnotta@gmail.com>
2022-05-27 09:42:10 -0300

    add list of favorites games

--- src/app/template/dynamics/dynamics.component.ts
+++ src/app/template/dynamics/dynamics.component.ts
@@ -1,11 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Person {
+  name: string;
+  favorites: Favorite[];
+}
+interface Favorite {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'app-dynamics',
   templateUrl: './dynamics.component.html',
   styleUrls: ['./dynamics.component.css']
 })
 export class DynamicsComponent implements OnInit {
+
+  person: Person = {
+    name: 'John',
+    favorites: [
+      {
+        id: 1,
+        name: 'RDR2'
+      },
+      {
+        id: 2,
+        name: 'CSGO'
+      },
+    ]
+  }
 
   constructor() { }
 
