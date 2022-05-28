e5a71bd7c81b3501ce5742eb7fe47d3c0980e9e7
Franco Pagnotta <francoapagnotta@gmail.com>
2022-05-28 19:19:44 -0300

    create dynamics reactive form with validations

--- src/app/reactive/dynamics/dynamics.component.ts
+++ src/app/reactive/dynamics/dynamics.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dynamics',
@@ -6,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dynamics.component.css']
 })
 export class DynamicsComponent implements OnInit {
+  myForm!: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.createForm();
+  }
+
+  createForm(): void {
+    this.myForm = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.minLength(4)]]
+    });
+  }
+
+  isInvalid(control: string) {
+    return this.myForm.controls[control].errors 
+      && this.myForm.controls[control].touched;
+  }
+
+  save(): void {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+    } else {
+      this.myForm.reset();
+      console.log(this.myForm.value);
+    }
   }
 
 }
