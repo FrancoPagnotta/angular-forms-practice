813ff435ff27449c279bb3a551febd740f977282
Franco Pagnotta <francoapagnotta@gmail.com>
2022-05-28 16:22:21 -0300

    create the reactive form using form builder service

--- src/app/reactive/basics/basics.component.ts
+++ src/app/reactive/basics/basics.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-basics',
@@ -7,15 +7,25 @@ import { FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./basics.component.css']
 })
 export class BasicsComponent implements OnInit {
-  myForm: FormGroup = new FormGroup({
-    product: new FormControl(''),
-    price: new FormControl(''),
-    stock: new FormControl(''),
-  });
+  // myForm: FormGroup = new FormGroup({
+  //   product: new FormControl(''),
+  //   price: new FormControl(''),
+  //   stock: new FormControl(''),
+  // });
+  myForm!: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.createForm();
+  }
+
+  createForm(): void {
+    this.myForm = this.formBuilder.group({
+      product: [''],
+      price: [0],
+      stock: [0]
+    });
   }
 
 }
