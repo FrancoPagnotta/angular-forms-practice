f0d0833be6441ce5c0cfe389f8adad9df7fcf799
Franco Pagnotta <francoapagnotta@gmail.com>
2022-05-28 16:16:08 -0300

    create reactive form

--- src/app/reactive/basics/basics.component.ts
+++ src/app/reactive/basics/basics.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-basics',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./basics.component.css']
 })
 export class BasicsComponent implements OnInit {
+  myForm: FormGroup = new FormGroup({
+    product: new FormControl(''),
+    price: new FormControl(''),
+    stock: new FormControl(''),
+  });
 
   constructor() { }
 
